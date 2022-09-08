083230e0963b9adfdfc255bf993b9bb119af69d8
Temitope Adebayo <tonikky01@gmail.com>
2022-09-08 14:09:55 -0700

    Write a C program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <studio.h>
+
+/**
+ *
+ *main - Emtry point
+ *
+ */
+int main(void)
+{
+	char new_string[] = "\"programming is like building a multilingual puzzle"/
+
+           puts(new_string);
+
+           return(0);
+}	   
