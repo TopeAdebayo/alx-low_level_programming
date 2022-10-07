89c02e7377fbcc24ef81e5eaf4795967b8252a76
Temitope Adebayo <tonikky01@gmail.com>
2022-10-06 20:31:39 -0700

    Write a program that multiplies two positive numbers.

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,27 +1,60 @@
+#include "main.h"
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 
 /**
- * main - Check the code
- * @argc: number of arguments
- * @argv: arguments
- * Return: Nothing
+ * print_error - Print errors and exit
+ *
+ * Return: void
  */
+void print_error(void)
+{
+	printf("Error\n");
+	exit(98);
+}
+
+/**
+ * is_digit - checks if a string contains a non-digit char
+ * @s: string to be evaluated
+ *
+ * Return: 0 if a non-digit is found, 1 otherwise
+ */
+int is_digit(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		if (s[i] < '0' || s[i] > '9')
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+/**
+ * main - Entry point
+ * @argc: Argument count
+ * @argv: Argument vector
+ *
+ * Return: Always zero
+ */
+
 int main(int argc, char *argv[])
 {
-	int num1, num2, result;
+	int mul = 0;
 
 	if (argc != 3)
+		print_error();
+
+	else if (is_digit(argv[1]) && is_digit(argv[2]))
 	{
-		printf("Error\n");
-		exit(98);
+		mul = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", mul);
 	}
 	else
-	{
-		num1 = atoi(argv[1]);
-		num2 = atoi(argv[2]);
-		result = num1 * num2;
-		printf("%d\n", result);
-		return (0);
-	}
+		print_error();
+
+	return (0);
 }
